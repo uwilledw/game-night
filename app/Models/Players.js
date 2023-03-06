f3e2c8b01939eb1468089f481fa2b9e1ed1cbae4
William Edwards <williamedwards7717@gmail.com>
2023-03-06 14:23:01 -0700

    begin

---
+++ app/Models/Players.js
@@ -0,0 +1,18 @@
+
+
+export class Player {
+    // name
+    // level
+    // color
+
+    constructor(name, score, color) {
+        this.name == name
+        this.score == score
+        this.color == color
+    }
+
+
+    check() {
+        console.log(`Hi my name is ${this.name}, my score ${this.score} and ${this.color}`)
+    }
+}
